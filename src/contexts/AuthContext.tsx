c4efb4e316b4dc79b2e66f05cd84c6ca5c829e19
mithilesh-1405 <mithileshmy14@gmail.com>
2026-05-30 21:16:08 +0530

    select account prompt added

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -53,7 +53,10 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/dashboard`
+        redirectTo: `${window.location.origin}/dashboard`,
+        queryParams: {
+          prompt: 'select_account',
+        }
       }
     });
 
